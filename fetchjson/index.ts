ca79e79176cf14edab97c74e00e8144cac36e90f
cvssea <42877793+cvssea@users.noreply.github.com>
2020-02-20 14:18:16 +0200

    hello world

---
+++ fetchjson/index.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then(res => {
+  const todo = res.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean): void => {
+  console.log(` id: ${id} - title: ${title} - completed: ${completed}`);
+};
+
+// usage: incorrect object keys
+// usage: incorrect order of arguments
